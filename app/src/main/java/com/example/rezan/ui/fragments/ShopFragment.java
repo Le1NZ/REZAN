193c05d78e67b2c7790b5e00162ef1c776397f86
Le1NZ <pokrovskiypasha@yandex.ru>
2024-03-27 02:11:31 +0300

    Add function of adding products

--- app/src/main/java/com/example/rezan/ui/fragments/ShopFragment.java
+++ app/src/main/java/com/example/rezan/ui/fragments/ShopFragment.java
@@ -1,17 +1,20 @@
 package com.example.rezan.ui.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.rezan.R;
 import com.example.rezan.data.db.Product;
 import com.example.rezan.databinding.FragmentShopBinding;
 import com.example.rezan.ui.adapters.ProductsAdapter;
+import com.example.rezan.ui.viewModels.ShopFragmentViewModel;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,10 +22,13 @@ public class ShopFragment extends Fragment {
 
     private ProductsAdapter adapter;
     static public FragmentShopBinding binding;
+    private ShopFragmentViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentShopBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this).get(ShopFragmentViewModel.class);
+        viewModel.isUserAdmin();
 
         binding.shopRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -35,6 +41,18 @@ public class ShopFragment extends Fragment {
         binding.shopRecycler.setAdapter(adapter);
         binding.shopRecycler.setItemAnimator(null);
         adapter.startListening();
+
+
+        viewModel.isUserAdmin.observe(getViewLifecycleOwner(), isAdmin -> {
+            if (isAdmin) {
+                binding.addNewProduct.setVisibility(View.VISIBLE);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        binding.addNewProduct.setOnClickListener(v -> {
+            Navigation.findNavController(binding.getRoot()).navigate(R.id.action_navigation_shop_to_addProductFragment);
+        });
 
         return binding.getRoot();
     }
